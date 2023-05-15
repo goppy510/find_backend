de01234d8ad5d04d874e9e6d7480c85c7c8c9575
goppy <tacumin.goppy.06@gmail.com>
2023-05-15 22:56:35 +0900

    テストの実行を変えた

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include FactoryBot::Syntax::Methods
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -56,8 +58,24 @@ RSpec.configure do |config|
   # https://rspec.info/features/6-0/rspec-rails
   config.infer_spec_type_from_file_location!
 
+  config.include ActiveSupport::Testing::TimeHelpers
+  config.include ActiveJob::TestHelper
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DataBaseCleaner.strategy = :truncation
+    DataBaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DataBaseCleaner.start
+  end
+
+  config.after(:each) do
+    DataBaseCleaner.clean
+  end
 end

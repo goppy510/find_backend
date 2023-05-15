3e85f322f2273b33ac6b4761a85d1ea8e6a44736
goppy <tacumin.goppy.06@gmail.com>
2023-05-15 22:20:22 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :users do
+      post '/signup',                        to: 'api/user#signup'
+      post '/submission_verify_mail/:token', to: 'api/user#submit_verify_email'
+      post '/activation',                    to: 'api/user#activate_account'
+      post '/login',                         to: 'api/user#login'
+      delete '/logout',                      to: 'api/user#logout'
+      get '/:id',                            to: 'api/user#show'
+      put '/:id/password',                   to: 'api/user#update_password'
+      put '/:id/profile',                    to: 'api/user#update_profile'
+    end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    namespace :prompts do
+      get '/',       to: 'api/prompts#index'
+      post '/',      to: 'api/prompts#create'
+      get '/:id',    to: 'api/prompts#show'
+      put '/:id',    to: 'api/prompts#update'
+      delete '/:id', to: 'api/prompts#destroy'
+      post '/:id/like', to: 'prompts#like'
+      delete '/:id/like', to: 'prompts#unlike'
+    end
+  end
 end

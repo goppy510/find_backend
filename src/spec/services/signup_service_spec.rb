6ef6c397ce9a34b5e733867e4c3b21b8e690e725
goppy <tacumin.goppy.06@gmail.com>
2023-05-20 21:51:24 +0900

    test changed

--- src/spec/services/signup_service_spec.rb
+++ src/spec/services/signup_service_spec.rb
@@ -5,17 +5,31 @@ require 'rspec-rails'
 require 'faker'
 
 describe SignupService do
-  describe '#self.signup' do
+  describe '#signup' do
     context '正常系' do
       context '正しいメールアドレスとパスワードを受け取った場合' do
+        before do
+          travel_to Time.zone.local(2023, 05, 10, 3, 0, 0)
+        end
+
         let!(:email) { Faker::Internet.email }
         let!(:password) { 'P@ssw0rd' }
 
-        it 'usersにインサートされること' do
-          SignupService.signup(email, password)
+        it 'usersにメールアドレスとハッシュ化されたパスワードがインサートされること' do
+          service = SignupService.new(email, password)
+          service.signup
           user = User.find_by(email: email)
           expect(user.email).to eq(email)
           expect(user.authenticate(password)).to be_truthy
+        end
+
+        it 'RegistrationTokensに当該ユーザーID、トークン、トークンの有効期限がインサートされること' do
+          service = SignupService.new(email, password)
+          service.signup
+          user = User.find_by(email: email)
+          registration_token = RegistrationToken.find_by(user_id: user.id)
+          expect(registration_token.token).to_not eq nil
+          expect(registration_token.expires_at).to eq(Time.zone.local(2023, 05, 10, 4, 0, 0))
         end
       end
     end
@@ -25,7 +39,7 @@ describe SignupService do
         let!(:password) { 'P@ssw0rd' }
 
         it 'ArgumentErrorがスローされること' do
-          expect {  SignupService.signup(nil, password) }.to raise_error(ArgumentError, 'emailがありません')
+          expect {  SignupService.new(nil, password) }.to raise_error(ArgumentError, 'emailがありません')
         end
       end
 
@@ -33,7 +47,7 @@ describe SignupService do
         let!(:email) { Faker::Internet.email }
 
         it 'ArgumentErrorがスローされること' do
-          expect {  SignupService.signup(email, nil) }.to raise_error(ArgumentError, 'passwordがありません')
+          expect {  SignupService.new(email, nil) }.to raise_error(ArgumentError, 'passwordがありません')
         end
       end
     end

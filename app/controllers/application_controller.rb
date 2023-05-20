724c575235dee68bf1fde49f411b4c4092f4ac85
goppy <tacumin.goppy.06@gmail.com>
2023-05-20 13:38:33 +0900

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_token
+
+  extend T::Sig
+
+  # エラーをjsonで返すメソッド
+  sig { params(status: Integer, resource: String, code: String).void }
+  def render_error(status = 400, resource, code)
+    message = I18n.t("errors.#{resource}.#{code}")
+    render json: { error: { status: status, code: code,  message: message }, status: status
+  end
+
+  private
+
+  def authenticate_token
+    # トークンのチェックを行うロジックを実装
+    # トークンが無効な場合は適切な処理を行う（リダイレクト、エラーレスポンスなど）
+  end
 end

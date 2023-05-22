3128279a6a898bdca95052471530e3e0021f0696
goppy <tacumin.goppy.06@gmail.com>
2023-05-22 17:32:29 +0900

    change

--- src/config/settings_loader.rb
+++
@@ -1,7 +0,0 @@
-require 'settingslogic'
-
-# Define a Settings class to load the settings files
-class Settings < Settingslogic
-  source "#{Rails.root}/config/settings/#{Rails.env}.yml"
-  namespace Rails.env
-end

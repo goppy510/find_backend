1a28ffea39f9f5a578272f1b5e156996bbcd80ca
goppy <tacumin.goppy.06@gmail.com>
2023-05-22 10:21:37 +0900

    regsitration_tokensのfactoryを修正

--- src/spec/factories/registration_token.rb
+++ src/spec/factories/registration_token.rb
@@ -2,11 +2,13 @@
 
 require 'factory_bot_rails'
 require 'faker'
+require 'securerandom'
 
 FactoryBot.define do
   factory :registration_token do
     sequence(:id) { Faker::Number.between(1,20) }
     sequence(:user_id) { Faker::Number.between(1,20) }
-    sequence(:expires_at) { |n| "password_digest_#{n}" }
+    sequence(:token) { SecureRandom.uuid }
+    sequence(:expires_at) { Time.current.in_time_zone + 1.hour }
   end
 end

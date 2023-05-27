cc223296249b64b0d944bbc279b01b3faf6ce906
goppy <tacumin.goppy.06@gmail.com>
2023-05-27 21:20:29 +0900

    add

--- src/app/services/auth/tokenizable_service.rb
+++ src/app/services/auth/tokenizable_service.rb
@@ -2,41 +2,17 @@
 
 require 'jwt'
 
+# Auth::AuthTokenServiceの上位
 module Auth::TokenizableService
 
   def self.included(base)
     base.extend ClassMethods
   end
 
-  ## instance method
-  # トークンを返す
-  def to_token
-    Auth::AuthTokenService.new(payload: to_token_payload).token
-  end
-
   # 有効期限付きのトークンを返す
+  # メール本文やフロントに渡す「2時間以内に下記URLへアクセスしてください」のようなメッセージに埋め込むｔめのもの
   def to_lifetime_token(lifetime)
-    auth = Auth::AuthTokenService.new(lifetime: lifetime, payload: to_token_payload)
+    auth = Auth::AuthTokenService.new(lifetime: lifetime, payload: { sub: id })
     { token: auth.token, lifetime_text: auth.lifetime_text }
-  end
-
-  private
-
-  def to_token_payload
-    { sub: id }
-  end
-
-  ## class method
-  module ClassMethods
-    def from_token(token)
-      auth_token = Auth::AuthTokenService.new(token: token)
-      from_token_payload(auth_token.payload)
-    end
-
-    private
-
-    def from_token_payload(payload)
-      find(payload[:sub])
-    end
   end
 end

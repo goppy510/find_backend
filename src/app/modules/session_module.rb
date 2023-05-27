ac406c9b669bd30b704fcea121b84be856a7971c
goppy <tacumin.goppy.06@gmail.com>
2023-05-27 18:58:47 +0900

    add

--- src/app/modules/session_module.rb
+++ src/app/modules/session_module.rb
@@ -1,45 +1,48 @@
-class SessionService
-  attr_reader :email, :password, :token
-
-  def initialize(email: nil, password: nil, token: nil)
-    # ログイン後の
-    if email.blank? && password.blank? and token.present?
-    end
-    @email = Account::Email.from_string(email)
-    @password = Accoount::Password.from_string(password)
-  end
+module Session
 
   def login
-    activated_user = entity(self.email, self.password)
+    email = Email.from_string(params[:email])
+    password = Password.from_string(params[:password])
+
+    activated_user = UserRepository.find_by_activated(email, password)
 
     # プロフィール未入力の場合はその旨をjsonで返す（フロント側で入力画面に飛ばすため）
-    return unless profile_created?(activated_user)
+    render_error(400, 'user', 'profile_incomplete') unless ProfileRepository.find_by_user_id(activated_user.id)
 
     # api認証用のtokenを生成する
-    auth = activated_user.token_generate
+    auth = activated_user.genrate_token(activated_user)
+
+    # クッキーのtoken等を入れる
+    cookies[token_access_key] = save_token_cookie
 
     # ハッシュ形式にして呼び出し元に返す
-    res = {}
-    res[:user] = {
+    res = {
       exp: auth.payload[:exp],
-      user: activated_user.id
+      user_id: activated_user.id
     }
-    res[:auth] = {
-      token_for_cookie(auth)
-    }
-    res
+    render json: { res }
+  end
+
+  # ログアウト
+  def logout
+    Auth::AuthenticatorService.new.delete_cookie
+    head(:ok)
+  end
+
+  # トークンが有効ならUserオブジェクトを返す
+  def authenticate_user
+    res = Auth::AuthenticatorService.new.authenticate_user
+    render json: { res }
   end
 
   private
 
-  def entity(email, password)
-    activated_user ||= User.find_activated(self.email, self.password)
-    raise UserNotFound, '有効なユーザーが見つかりませんでした' unless activated_user
-    activated_user
+  def genrate_token(user)
+    Auth::AuthTokenService.new(payload: { sub: user.id })
   end
 
   # クッキーに保存するトークン
-  def token_for_cookie(auth)
+  def save_token_cookie(auth)
     {
       value: auth.token,
       expires: Time.at(auth.payload[:exp]),
@@ -47,18 +50,4 @@ class SessionService
       http_only: true
     }
   end
-
-  # emailからアクティブなユーザーを返す
-  def find_activated(email, password)
-    user = User.find_by(email: email, activated: true)
-    return user ? user&.authenticate(password) : false
-  end
-
-  # ユーザープロフィールが登録されている場合trueを返す
-  def profile_created?(activated_user)
-    Profile.exists?(user_id: activated_user.id)
-  end
 end
-
-class UserNotFound < StandardError; end
-class InvalidUserError < StandardError; end

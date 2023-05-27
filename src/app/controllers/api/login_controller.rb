8bacd3194dd6369c8ce62fb64f21fc62e4a12d2a
goppy <tacumin.goppy.06@gmail.com>
2023-05-27 12:54:58 +0900

    change

--- src/app/controllers/api/login_controller.rb
+++ src/app/controllers/api/login_controller.rb
@@ -1,2 +1,21 @@
-class LoginController < ApplicationController
+#frozen_string_literal: true
+
+class Api::LoginController < ApplicationController
+  before_action :delete_cookie
+  before_action :authenticate, only: [:login]
+
+  # ログイン
+  def create
+    service = LoginService.new(params[:email], params[:password])
+    res = service.login
+    render_error(:unauthorized, :user, :unauthorized) unless res
+
+    cookies[token_access_key] = res[:auth]
+    render json: { res[:user] }
+  end
+
+  # ログアウト
+  def destroy
+    head(:ok)
+  end
 end

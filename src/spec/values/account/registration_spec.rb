17743b04c5f998771f9bb7d4f0564fcd372298dd
goppy <tacumin.goppy.06@gmail.com>
2023-05-27 10:39:49 +0900

    fix

--- src/spec/values/account/registration_spec.rb
+++ src/spec/values/account/registration_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 require 'rspec-rails'
 
-describe Registration do
+describe Account::Registration do
   describe '#token' do
     context '正常系' do
       it 'トークンが生成されること' do
-        token = Registration.token
+        token = Account::Registration.token
         expect(token).not_to be_nil
         expect(token).to match(/\A[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\z/i)
       end
@@ -21,7 +21,7 @@ describe Registration do
       end
 
       it '有効期限がが生成されること' do
-        expires_at = Registration.expires_at
+        expires_at = Account::Registration.expires_at
         expect(expires_at).to eq(Time.zone.local(2023, 05, 10, 4, 0, 0))
       end
     end

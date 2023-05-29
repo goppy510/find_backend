dd8e6b1ab071559a9ef3859befb36b62f35b63a3
goppy <tacumin.goppy.06@gmail.com>
2023-05-29 13:20:57 +0900

    add

--- src/app/repositories/user_repository.rb
+++ src/app/repositories/user_repository.rb
@@ -3,16 +3,20 @@
 class UserRepository
   class << self
 
-    def create(email, passsword)
+    def create(email, password)
       User.create!(email: email, password: password)
     end
 
     def find_by_id(id)
-      User.find(id)
+      User.find_by(id:id, activated: true)
+    end
+
+    def find_by_email_not_activated(email)
+      User.find_by(email: email, activated: false)
     end
 
     def find_by_email(email)
-      User.find_by(email: email)
+      User.find_by(email: email, activated: true)
     end
 
     def find_by_activated(email, password)

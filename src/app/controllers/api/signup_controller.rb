74cca09792b3ed65631e5dbc0786df31590ba6ea
goppy <tacumin.goppy.06@gmail.com>
2023-05-29 13:20:21 +0900

    editted

--- src/app/controllers/api/signup_controller.rb
+++ src/app/controllers/api/signup_controller.rb
@@ -1,17 +1,25 @@
 #frozen_string_literal: true
 
 class Api::SignupController < ApplicationController
+  include SessionModule
+
   # 仮登録用
   def signup
-    if params[:email].blank? || params[:password].blank?
+    if signup_params[:email].blank? || signup_params[:password].blank?
       render_error(400, 'user', 'invalid_parameter')
       return
     end
 
-    service = SignupService.new(params[:email], params[:password])
+    service = SignupService.new(signup_params[:email], signup_params[:password])
     service.signup
     service.activation_email
 
     render json: { status: 'success' }, status: 200
+  end
+
+  private
+
+  def signup_params
+    params.permit(:email, :password)
   end
 end

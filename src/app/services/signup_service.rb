f288bd05e4485e33706c4a663e9e204a9eaac4a7
goppy <tacumin.goppy.06@gmail.com>
2023-05-29 13:09:12 +0900

    仮登録時のtokenをJWTにした

--- src/app/services/signup_service.rb
+++ src/app/services/signup_service.rb
@@ -1,40 +1,52 @@
 #frozen_string_literal: true
 
 class SignupService
+  include SessionModule
+
   attr_reader :email, :password, :token, :expires_at
 
   def initialize(email, password)
     raise ArgumentError, 'emailまたはpasswordがありません' if email.blank? or password.blank?
-
     @email = Account::Email.from_string(email)
     @password = Account::Password.from_string(password)
-    @token = Account::Registration.token
-    @expires_at = Account::Registration.expires_at
 
     self.freeze
   end
 
   # ユーザー情報をDBに登録する
   def signup
-    ActiveRecord::Base.transaction do
-      user = UserRepository.create(@email, @password)
-      RegistrationTokenRepository.create(user.id, @token, @expires_at)
-    end
+    UserRepository.create(@email, @password)
   rescue ActiveRecord::RecordInvalid => e
     raise SignupError, e.message
   end
 
   # 本登録用のメールを送信する
   def activation_email
-    user = UserUserRepository.find_by_email(@email)
-    registration_token = RegistrationTokenRepository.find_by_user_id(user.id)
 
-    raise RecordNotFound, 'userまたはregistration_tokenがありません' if user.blank? || registration_token.blank?
+    user = UserRepository.find_by_email_not_activated(@email)
+    raise UserNotFound, '指定のユーザーが見つかりませんでした' unless user
+
+    # アクティベーションメールのリンクのクエリパラメータに入れるJWTを生成する
+    payload = signup_payload(user)
+    auth = generate_token(payload) #SessionModuleのメソッド
+    token = auth.token
+    expires_at = toTime(auth) #SessionModuleのメソッド
 
     # メールの作成と送信
-    RegistrationMailer.send_activation_mail(@email, @token, @expires_at).deliver
+    ActivationMailer.send_activation_email(email, token, expires_at).deliver
+  end
+
+  private
+
+  # アクティベーション用のトークン生成のためのペイロード
+  def signup_payload(user)
+    {
+      sub: user.id,
+      type: 'activation'
+    }
   end
 end
 
 class SignupError < StandardError; end
+class UserNotFound < StandardError; end
 class SubmitVerifyEmailError < StandardError; end
--- src/spec/services/signup_service_spec.rb
+++ src/spec/services/signup_service_spec.rb
@@ -17,19 +17,9 @@ describe SignupService do
 
         it 'usersにメールアドレスとハッシュ化されたパスワードがインサートされること' do
           service = SignupService.new(email, password)
-          service.signup
-          user = User.find_by(email: email)
+          user = service.signup
           expect(user.email).to eq(email)
           expect(user.authenticate(password)).to be_truthy
-        end
-
-        it 'RegistrationTokensに当該ユーザーID、トークン、トークンの有効期限がインサートされること' do
-          service = SignupService.new(email, password)
-          service.signup
-          user = User.find_by(email: email)
-          registration_token = RegistrationToken.find_by(user_id: user.id)
-          expect(registration_token.token).to_not eq nil
-          expect(registration_token.expires_at).to eq(Time.zone.local(2023, 05, 10, 4, 0, 0))
         end
       end
     end
@@ -50,21 +40,48 @@ describe SignupService do
           expect { SignupService.new(email, nil) }.to raise_error(ArgumentError, 'emailまたはpasswordがありません')
         end
       end
+    end
+  end
 
-      context 'regitration_tokensへのインサートに失敗した場合' do
-        let(:email) { 'test@example.com' }
-        let(:password) { 'P@ssw0rd' }
-    
-        it 'usersとregistration_tokensにデータがインサートされないこと' do
-          allow(RegistrationToken).to receive(:create!).and_raise(ActiveRecord::RecordInvalid)
-          expect {
-            SignupService.new(email, password).signup
-          }.to raise_error(SignupError)
-    
-          # ユーザーと登録トークンが両方とも作成されていないことを確認する
-          user = User.find_by(email: email)
-          expect(user).to be_nil
-          expect(RegistrationToken.count).to be_zero
+  describe '#activation_email' do
+    context '正常系' do
+      let!(:mailer) { double('ActionMailer') }
+      before do
+        allow(mailer).to receive(:deliver)
+        allow(ActivationMailer).to receive(:send_activation_email).and_return(mailer)
+      end
+
+      context '存在するuserかつアクティベーションされていないuserの場合' do
+        before do
+          travel_to Time.zone.local(2023, 05, 10, 3, 0, 0)
+        end
+
+        let!(:email) { Faker::Internet.email }
+        let!(:password) { 'P@ssw0rd' }
+        let!(:user) { create(:user, email: email, password: password) }
+
+        it 'メールが送られること' do
+          service = SignupService.new(email, password)
+          service.activation_email
+          expect(ActivationMailer).to have_received(:send_activation_email)
+          expect(mailer).to have_received(:deliver)
+        end
+      end
+    end
+
+    context '異常系' do
+      context 'userがアクティベート済の場合' do
+        before do
+          travel_to Time.zone.local(2023, 05, 10, 3, 0, 0)
+        end
+
+        let!(:email) { Faker::Internet.email }
+        let!(:password) { 'P@ssw0rd' }
+        let!(:user) { create(:user, email: email, password: password, activated: true) }
+
+        it 'UserNotFoundがスローされること' do
+          service = SignupService.new(email, password)
+          expect { service.activation_email }.to raise_error(UserNotFound)
         end
       end
     end

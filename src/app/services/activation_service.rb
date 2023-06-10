8768912786aca917fde517bfec5e55dbba036f0d
goppy <tacumin.goppy.06@gmail.com>
2023-06-10 15:58:41 +0900

    fix

--- src/app/services/activation_service.rb
+++ src/app/services/activation_service.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 # ユーザーがアクティベーションメールのリンクをクリックしたらアカウントを有効化するためのもの
 class ActivationService
@@ -10,18 +10,20 @@ class ActivationService
 
   # アクティベート
   def activate
-    auth = authenticate_user_not_activate(@token) #SessionModuleのメソッド
+    auth = authenticate_user_not_activate(@token) # SessionModuleのメソッド
     raise AuthenticationError unless auth
 
-    #アクティベートする
+    # アクティベートする
     user = UserRepository.find_by_id(auth[:user_id])
-    raise UserNotFound if user.blank? or user&.activated
+    raise UserNotFound if user.blank? || user&.activated
+
     UserRepository.activate(user)
   end
 
   class << self
     def activate(token)
       raise ArgumentError, 'tokenがありません' unless token
+
       service = new(token)
       service&.activate
     end

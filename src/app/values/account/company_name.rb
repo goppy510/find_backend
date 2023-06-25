b00fd779ce45244f3e25c1d8a49e11afe192ec9d
goppy <tacumin.goppy.06@gmail.com>
2023-06-25 10:00:10 +0900

    add

---
+++ src/app/values/account/company_name.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Account
+  class CompanyName
+    attr_reader :value
+
+    LENGTH = 100
+
+    def initialize(value)
+      raise ArgumentError, 'compnay_nameがありません' unless value
+      raise FormatError, '形式が正しくありません' unless company_name_valid?(value)
+
+      @value = value
+
+      freeze
+    end
+
+    private
+
+    def company_name_valid?(value)
+      value.length <= LENGTH
+    end
+
+    class << self
+      def from_string(value)
+        new(value).value.to_s
+      end
+    end
+  end
+end
+
+class FormatError < StandardError; end

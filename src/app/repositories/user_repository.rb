b93978328f01422665e9bc99bdd3c7699028e0f5
goppy <tacumin.goppy.06@gmail.com>
2023-06-25 11:19:35 +0900

    fix

--- src/app/repositories/user_repository.rb
+++ src/app/repositories/user_repository.rb
@@ -1,22 +1,21 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 class UserRepository
   class << self
-
     def create(email, password)
-      User.create!(email: email, password: password)
+      User.create!(email:, password:)
     end
 
     def find_by_id(id)
-      User.find_by(id: id)
+      User.find_by(id:)
     end
 
     def find_by_email(email)
-      User.find_by(email: email)
+      User.find_by(email:)
     end
 
     def find_by_activated(email, password)
-      user = User.find_by(email: email, activated: true)
+      user = User.find_by(email:, activated: true)
       return user if user&.authenticate(password)
     end
 

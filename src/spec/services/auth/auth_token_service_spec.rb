011cb851c499be1dafbd3f3a535fd1c5c27e8484
goppy <tacumin.goppy.06@gmail.com>
2023-06-25 09:33:15 +0900

    fix for rubocop

--- src/spec/services/auth/auth_token_service_spec.rb
+++ src/spec/services/auth/auth_token_service_spec.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'rails_helper'
 require 'rspec-rails'
@@ -8,7 +8,7 @@ describe Auth::AuthTokenService do
   describe '#new' do
     context '正常系' do
       before do
-        travel_to Time.zone.local(2023, 05, 10, 3, 0, 0)
+        travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
       end
 
       context 'tokenが存在しない場合' do
@@ -55,14 +55,14 @@ describe Auth::AuthTokenService do
         let!(:token) { Auth::AuthTokenService.new.token }
 
         it '@payloadのaudがAPIホストであること,expが2023-05-24 03:00:00のタイムスタンプであること' do
-          service = Auth::AuthTokenService.new(token: token)
+          service = Auth::AuthTokenService.new(token:)
           payload = service.instance_variable_get(:@payload)
           expect(payload[:exp]).to eq(Time.parse('2023-05-24 03:00:00').to_i)
           expect(payload[:aud]).to eq(Settings[:app][:host])
         end
 
         it '@tokenが入力されたものと一致すること' do
-          service = Auth::AuthTokenService.new(token: token)
+          service = Auth::AuthTokenService.new(token:)
           token = service.instance_variable_get(:@token)
           expect(token).not_to be_nil
         end
@@ -73,7 +73,7 @@ describe Auth::AuthTokenService do
   describe '#find_available_user' do
     context '正常系' do
       before do
-        travel_to Time.zone.local(2023, 05, 10, 3, 0, 0)
+        travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
       end
 
       let!(:user1) { create(:user, activated: true) }
@@ -83,17 +83,19 @@ describe Auth::AuthTokenService do
         let!(:token) { Auth::AuthTokenService.new(payload: { sub: user1.id }).token }
 
         it 'user.idがsubのvalueと一致すること' do
-          service = Auth::AuthTokenService.new(token: token)
+          service = Auth::AuthTokenService.new(token:)
           actual_user = service.find_available_user
           expect(actual_user.id).to eq user1.id
         end
       end
 
       context 'usersにレコードがあり、かつ、tokenが無効な場合' do
-        let!(:token) { Auth::AuthTokenService.new(payload: { sub: user1.id, exp: Time.zone.local(2023, 04, 10, 3, 0, 0).to_i }).token }
+        let!(:token) do
+          Auth::AuthTokenService.new(payload: { sub: user1.id, exp: Time.zone.local(2023, 4, 10, 3, 0, 0).to_i }).token
+        end
 
         it 'JWT::ExpiredSignatureが発生すること' do
-          expect { Auth::AuthTokenService.new(token: token)}.to raise_error(JWT::ExpiredSignature)
+          expect { Auth::AuthTokenService.new(token:) }.to raise_error(JWT::ExpiredSignature)
         end
       end
     end
@@ -102,7 +104,7 @@ describe Auth::AuthTokenService do
   describe '#find_not_available_user' do
     context '正常系' do
       before do
-        travel_to Time.zone.local(2023, 05, 10, 3, 0, 0)
+        travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
       end
 
       let!(:user1) { create(:user) }
@@ -112,7 +114,7 @@ describe Auth::AuthTokenService do
         let!(:token) { Auth::AuthTokenService.new(payload: { sub: user1.id }).token }
 
         it 'user.idがsubのvalueと一致すること' do
-          service = Auth::AuthTokenService.new(token: token)
+          service = Auth::AuthTokenService.new(token:)
           actual_user = service.find_not_available_user
           expect(actual_user.id).to eq user1.id
         end
@@ -123,7 +125,7 @@ describe Auth::AuthTokenService do
   describe '#lifetime_text' do
     context '正常系' do
       before do
-        travel_to Time.zone.local(2023, 05, 10, 3, 0, 0)
+        travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
       end
 
       context '正しくauthが生成できた場合' do

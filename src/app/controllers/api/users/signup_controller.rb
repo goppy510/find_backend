603da6ce386d7cc7907f343ce07f1cb04efb5e1b
goppy <tacumin.goppy.06@gmail.com>
2023-06-25 10:31:53 +0900

    fix

--- src/app/controllers/api/users/signup_controller.rb
+++ src/app/controllers/api/users/signup_controller.rb
@@ -1,23 +1,27 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
-class Api::Users::SignupController < ApplicationController
-  include SessionModule
+module Api
+  module Users
+    class SignupController < ApplicationController
+      include SessionModule
 
-  # 仮登録用
-  def signup
-    if signup_params[:email].blank? || signup_params[:password].blank?
-      render_error(400, 'user', 'invalid_parameter')
-      return
+      # 仮登録用
+      def signup
+        if signup_params[:email].blank? || signup_params[:password].blank?
+          render_error(400, 'user', 'invalid_parameter')
+          return
+        end
+
+        SignupService.signup(signup_params[:email], signup_params[:password])
+
+        render json: { status: 'success' }, status: :ok
+      end
+
+      private
+
+      def signup_params
+        params.permit(:email, :password)
+      end
     end
-
-    SignupService.signup(signup_params[:email], signup_params[:password])
-
-    render json: { status: 'success' }, status: 200
-  end
-
-  private
-
-  def signup_params
-    params.permit(:email, :password)
   end
 end

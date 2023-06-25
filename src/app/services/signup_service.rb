c43a391a9b504f313114fb69671c425f90681874
goppy <tacumin.goppy.06@gmail.com>
2023-06-25 11:30:37 +0900

    fix

--- src/app/services/signup_service.rb
+++ src/app/services/signup_service.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 class SignupService
   include SessionModule
@@ -6,11 +6,12 @@ class SignupService
   attr_reader :email, :password, :token, :expires_at
 
   def initialize(email, password)
-    raise ArgumentError, 'emailまたはpasswordがありません' if email.blank? or password.blank?
+    raise ArgumentError, 'emailまたはpasswordがありません' if email.blank? || password.blank?
+
     @email = Account::Email.from_string(email)
     @password = Account::Password.from_string(password)
 
-    self.freeze
+    freeze
   end
 
   # ユーザー情報をDBに登録する
@@ -23,13 +24,13 @@ class SignupService
   # 本登録用のメールを送信する
   def activation_email
     user = UserRepository.find_by_email(@email)
-    raise Unauthorized, '指定のユーザーが見つかりませんでした' if user.blank? or user&.activated
+    raise Unauthorized, '指定のユーザーが見つかりませんでした' if user.blank? || user&.activated
 
     # アクティベーションメールのリンクのクエリパラメータに入れるJWTを生成する
     payload = signup_payload(user)
-    auth = generate_token(lifetime: Auth.token_signup_lifetime, payload: payload) #SessionModuleのメソッド
+    auth = generate_token(lifetime: Auth.token_signup_lifetime, payload:) # SessionModuleのメソッド
     token = auth.token
-    expires_at = Time.at(auth.payload[:exp]) #SessionModuleのメソッド
+    expires_at = Time.zone.at(auth.payload[:exp]) # SessionModuleのメソッド
 
     # メールの作成と送信
     ActivationMailer.send_activation_email(email, token, expires_at).deliver

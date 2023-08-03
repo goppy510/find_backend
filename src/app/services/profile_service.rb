7c48396b38873ab5a7b59cf9ff0b0699ac5a431f
goppy <tacumin.goppy.06@gmail.com>
2023-08-04 01:15:06 +0900

    add

--- src/app/services/profile_service.rb
+++ src/app/services/profile_service.rb
@@ -7,23 +7,24 @@ class ProfileService
               :profiles,
               :password
 
-  def initialize(user_id, profiles: nil, password: nil)
-    hash_profiles = profiles[:profiles]
+  def initialize(user_id, profiles: nil, current_password: nil, new_password: nil)
+    hash_profiles = profiles[:profiles] if profiles.present?
     @user_id = user_id
-    @password = password if password.present?
+    @current_password = current_password if current_password.present?
+    @new_password = new_password if new_password.present?
     @profiles = {}
-    @profiles[:name] = Account::Name.from_string(hash_profiles[:name]) if hash_profiles.key?(:name)
-    if hash_profiles.key?(:phone_number)
+    @profiles[:name] = Account::Name.from_string(hash_profiles[:name]) if hash_profiles&.key?(:name)
+    if hash_profiles&.key?(:phone_number)
       @profiles[:phone_number] = Account::PhoneNumber.from_string(hash_profiles[:phone_number])
     end
-    if hash_profiles.key?(:company_name)
+    if hash_profiles&.key?(:company_name)
       @profiles[:company_name] = Account::CompanyName.from_string(hash_profiles[:company_name])
     end
     # 以下、ラジオボタンの数値なのでバリデーションしない
-    @profiles[:employee_count] = hash_profiles[:employee_count] if hash_profiles.key?(:employee_count)
-    @profiles[:industry] = hash_profiles[:industry] if hash_profiles.key?(:industry)
-    @profiles[:position] = hash_profiles[:position] if hash_profiles.key?(:position)
-    @profiles[:business_model] = hash_profiles[:business_model] if hash_profiles.key?(:business_model)
+    @profiles[:employee_count] = hash_profiles[:employee_count] if hash_profiles&.key?(:employee_count)
+    @profiles[:industry] = hash_profiles[:industry] if hash_profiles&.key?(:industry)
+    @profiles[:position] = hash_profiles[:position] if hash_profiles&.key?(:position)
+    @profiles[:business_model] = hash_profiles[:business_model] if hash_profiles&.key?(:business_model)
 
     freeze
   end
@@ -34,13 +35,18 @@ class ProfileService
   end
 
   # プロフィール更新
-  def edit
-    ProfileRepository.edit(@user_id, @profiles)
+  def update_profiles
+    ProfileRepository.update_profiles(@user_id, @profiles)
   end
 
   # パスワード更新
   def update_password
-    ProfileRepository.update_service(@user_id, @password)
+    ProfileRepository.update_password(@user_id, @current_password, @new_password)
+  end
+
+  # プロファイル表示
+  def show
+    ProfileRepository.show(@user_id)
   end
 
   class << self
@@ -52,19 +58,40 @@ class ProfileService
       service&.create
     end
 
-    def edit(token, profiles: {})
-      raise ArgumentError, 'tokenがありません' if token.blank?
+    def update_profiles(user_id, profiles)
+      raise ArgumentError, 'user_idがありません' if user_id.blank?
+      raise ArgumentError, 'profilesがありません' if profiles.blank?
 
-      service = new(token, profiles)
-      service&.edit
+      service = new(user_id, profiles:)
+      service&.update_profiles
     end
 
-    def update_password(token, password:)
-      raise ArgumentError, 'tokenがありません' if token.blank?
-      raise ArgumentError, 'passwordがありません' if password.blank?
+    def update_password(user_id, current_password, new_password)
+      raise ArgumentError, 'user_idがありません' if user_id.blank?
+      raise ArgumentError, 'current_passwordがありません' if current_password.blank?
+      raise ArgumentError, 'new_passwordがありません' if new_password.blank?
 
-      service = new(token, password)
+      service = new(user_id, current_password:, new_password:)
       service&.update_password
+    rescue StandardError => e
+      Rails.logger.error e
+      raise e
+    end
+
+    def show(user_id)
+      raise ArgumentError, 'user_idがありません' if user_id.blank?
+
+      res = new(user_id)&.show
+      {
+        user_id: res[:user_id],
+        name: res[:full_name],
+        phone_number: res[:phone_number],
+        company_name: res[:company_name],
+        employee_count: EmployeeCount.find(res[:employee_count_id]).name,
+        industry: Industry.find(res[:industry_id]).name,
+        position: Position.find(res[:position_id]).name,
+        business_model: BusinessModel.find(res[:business_model_id]).name
+      }
     end
   end
 end

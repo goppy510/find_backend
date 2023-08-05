9d5616b7aaa8a7fb06b09d47d753c26798f6e420
goppy <tacumin.goppy.06@gmail.com>
2023-08-05 14:42:32 +0900

    change dir

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,24 +1,24 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :users do
-      post '/signup',                        to: 'api/signup#signup'
-      post '/activation',                    to: 'api/activation#activate'
-      post '/profile',                       to: 'api/profile#create'
-      put '/:id/profile',                    to: 'api/profile#update'
-      post '/login',                         to: 'api/login#create'
-      delete '/logout',                      to: 'api/login#destroy'
-      get '/:id/profile',                    to: 'api/profile#show'
-      put '/:id/password',                   to: 'api/profile#update_password'
-      post '/session',                       to: 'api/session#create'
-      delete '/session/:id',                 to: 'api/session#destroy'  
+      post '/signup',      to: 'signup#signup'
+      post '/activation',  to: 'activation#activate'
+      post '/profile',     to: 'profile#create'
+      put '/:id/profile',  to: 'profile#update'
+      post '/login',       to: 'login#create'
+      delete '/logout',    to: 'login#destroy'
+      get '/:id/profile',  to: 'profile#show'
+      put '/:id/password', to: 'profile#update_password'
     end
 
     namespace :prompts do
-      get '/',       to: 'api/prompts#index'
-      post '/',      to: 'api/prompts#create'
-      get '/:id',    to: 'api/prompts#show'
-      put '/:id',    to: 'api/prompts#update'
-      delete '/:id', to: 'api/prompts#destroy'
+      get '/',       to: 'prompts#index'
+      post '/',      to: 'prompts#create'
+      get '/:id',    to: 'prompts#show'
+      put '/:id',    to: 'prompts#update'
+      delete '/:id', to: 'prompts#destroy'
       post '/:id/like', to: 'prompts#like'
       delete '/:id/like', to: 'prompts#unlike'
     end

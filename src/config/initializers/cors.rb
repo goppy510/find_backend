4da8841e61d6db7d4f830f0abdb3bca86f9ca57d
goppy <tacumin.goppy.06@gmail.com>
2023-09-15 02:27:01 +0900

    change

--- src/config/initializers/cors.rb
+++ src/config/initializers/cors.rb
@@ -4,8 +4,10 @@ unless Rails.env.test?
   App::Application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       allow do
-        origins '*'
-        resource '*', headers: :any, methods: :any
+        origins 'http://localhost:8080'
+        resource '*',
+                  headers: :any,
+                  methods: %i[get post put patch delete options head]
       end
     end
   end

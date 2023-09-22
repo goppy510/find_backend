b84f9c183248b864f5af6dc48b6e83a7070a570e
goppy <tacumin.goppy.06@gmail.com>
2023-09-22 13:53:08 +0900

    fix

--- src/db/migrate/20230922033637_create_prompts.rb
+++ src/db/migrate/20230922033637_create_prompts.rb
@@ -4,12 +4,12 @@ class CreatePrompts < ActiveRecord::Migration[7.0]
   def change
     create_table :prompts do |t|
       t.string :uuid, null: false
-      t.references :category, foreign_key: true, null: false
+      t.references :category, foreign_key: true
       t.text :about
       t.text :input_example
       t.text :output_example
       t.text :prompt
-      t.references :generative_ai_model, foreign_key: true, null: false
+      t.references :generative_ai_model, foreign_key: true
       t.references :user, foreign_key: true, null: false
       t.boolean :deleted, null: false, default: false
 

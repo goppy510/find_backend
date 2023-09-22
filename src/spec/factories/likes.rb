8782c26b67833f4c3e57c6430c6e9f11601f0ad9
goppy <tacumin.goppy.06@gmail.com>
2023-09-22 14:03:16 +0900

    add

---
+++ src/spec/factories/likes.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'factory_bot_rails'
+require 'faker'
+
+FactoryBot.define do
+  factory :like do
+    association :user
+    association :prompt
+
+    # 任意でtimestamp等の属性を追加できます
+    created_at { Time.zone.now }
+    updated_at { Time.zone.now }
+  end
+end

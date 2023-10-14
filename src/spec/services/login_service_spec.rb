a6bbf58ed6e064b6052720bc668c82138bd2c704
goppy <tacumin.goppy.06@gmail.com>
2023-10-14 14:26:12 +0900

    change

--- src/spec/services/login_service_spec.rb
+++ src/spec/services/login_service_spec.rb
@@ -6,6 +6,7 @@ require 'faker'
 
 describe LoginService do
   include SessionModule
+
   describe '#login' do
     context '正常系' do
       context '正しいメールアドレスとパスワードを受け取った場合' do
@@ -15,8 +16,15 @@ describe LoginService do
 
         let!(:email) { Faker::Internet.email }
         let!(:password) { 'P@ssw0rd' }
+        let!(:logins) do
+          {
+            logins: {
+              email: email,
+              password: password
+            }
+          }
+        end
         let!(:user) { create(:user, email:, password:, activated: true) }
-        let!(:profile) { create(:profile, user_id: user.id) }
         let!(:payload) do
           {
             sub: user.id,
@@ -27,8 +35,7 @@ describe LoginService do
         let!(:token) { auth.token }
 
         it 'tokenとexpがレスポンスとして返ってくること' do
-          service = LoginService.new(email, password)
-          res = service.login
+          res = LoginService.login(logins)
           expect(res[:expires]).to eq(Time.zone.local(2023, 5, 24, 3, 0, 0))
           expect(res[:token]).to eq(token)
         end
@@ -38,17 +45,67 @@ describe LoginService do
     context '異常系' do
       context 'メールアドレスが引数にない場合' do
         let!(:password) { 'P@ssw0rd' }
+        let!(:logins) do
+          {
+            logins: {
+              email: nil,
+              password: password
+            }
+          }
+        end
 
         it 'ArgumentErrorがスローされること' do
-          expect { SignupService.new(nil, password) }.to raise_error(ArgumentError, 'emailまたはpasswordがありません')
+          expect { LoginService.login(logins) }.to raise_error(ArgumentError, 'emailがありません')
         end
       end
 
       context 'パスワードが引数にない場合' do
         let!(:email) { Faker::Internet.email }
+        let!(:logins) do
+          {
+            logins: {
+              email: email,
+              password: nil
+            }
+          }
+        end
 
         it 'ArgumentErrorがスローされること' do
-          expect { SignupService.new(email, nil) }.to raise_error(ArgumentError, 'emailまたはpasswordがありません')
+          expect { LoginService.login(logins) }.to raise_error(ArgumentError, 'passwordがありません')
+        end
+      end
+
+      context 'emailのフォーマットが不正な場合' do
+        let!(:email) { 'test' }
+        let!(:password) { 'P@ssw0rd' }
+        let!(:logins) do
+          {
+            logins: {
+              email: email,
+              password: password
+            }
+          }
+        end
+
+        it 'EmailFormatErrorがスローされること' do
+          expect { LoginService.login(logins) }.to raise_error(LoginService::EmailFormatError)
+        end
+      end
+
+      context 'passwordのフォーマットが不正な場合' do
+        let!(:email) { Faker::Internet.email }
+        let!(:password) { 'test' }
+        let!(:logins) do
+          {
+            logins: {
+              email: email,
+              password: password
+            }
+          }
+        end
+
+        it 'PasswordFormatErrorがスローされること' do
+          expect { LoginService.login(logins) }.to raise_error(LoginService::PasswordFormatError)
         end
       end
     end

c619f4612890758fb2791c4b7e0a8aad840bcd36
goppy <tacumin.goppy.06@gmail.com>
2023-10-16 20:17:38 +0900

    change

--- src/app/services/permission_service.rb
+++ src/app/services/permission_service.rb
@@ -10,24 +10,28 @@ class PermissionService
               :permissions
 
   def initialize(token,  permissions: {})
-    array_permissions = permissions[:permissions][:resource] if permissions.present? && permissions[:permissions].present? && permissions[:permissions][:resource].present?
+    permission = permissions[:permissions][:resource] if permissions.present? && permissions[:permissions].present? && permissions[:permissions][:resource].present?
     @target_user_id = permissions[:permissions][:target_user_id] if permissions.present? && permissions[:permissions].present? && permissions[:permissions][:target_user_id].present?
     @user_id = authenticate_user(token)[:user_id]
 
     @permissions = {}
-    @permissions[:resource] = array_permissions
+    @permissions[:resource] = permission
 
     freeze
   end
 
   # 権限追加
-  def upsert
-    PermissionRepository.upsert(@target_user_id, @permissions)
+  def create
+    PermissionRepository.create(@target_user_id, @permissions)
   end
 
   # 権限表示
   def show
     PermissionRepository.show(@target_user_id)
+  end
+
+  def delete
+    PermissionRepository.delete(@target_user_id, @permissions)
   end
 
   # 自分の権限表示
@@ -36,13 +40,13 @@ class PermissionService
   end
 
   class << self
-    def upsert(token, permissions)
+    def create(token, permissions)
       raise ArgumentError, 'tokenがありません' if token.blank?
       raise ArgumentError, 'permissionsがありません' if permissions.blank?
       raise Forbidden unless has_permisssion_role?(token)
 
       service = new(token, permissions:)
-      service&.upsert
+      service&.create
     end
 
     def show(token, permissions)
@@ -54,6 +58,15 @@ class PermissionService
       {
         resource: res
       }
+    end
+
+    def delete(token, permissions)
+      raise ArgumentError, 'tokenがありません' if token.blank?
+      raise ArgumentError, 'permissionsがありません' if permissions.blank?
+      raise Forbidden unless has_permisssion_role?(token)
+
+      service = new(token, permissions:)
+      service&.delete
     end
 
     def has_contract_role?(token)

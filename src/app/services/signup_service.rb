ae1ef77725f26e8651a9c8b937be4b886ebdf6bc
goppy <tacumin.goppy.06@gmail.com>
2023-10-16 22:18:29 +0900

    change

--- src/app/services/signup_service.rb
+++ src/app/services/signup_service.rb
@@ -4,7 +4,6 @@ class SignupService
   include SessionModule
 
   class DuplicateEntry < StandardError; end
-  class Unauthorized < StandardError; end
   class EmailFormatError < StandardError; end
   class PasswordFormatError < StandardError; end
 
@@ -13,10 +12,8 @@ class SignupService
               :user_id,
               :expires_at
 
-  def initialize(signups: nil, token: nil)
+  def initialize(signups: nil)
     hash_signups = signups[:signups] if signups.present?
-    hash_token = token[:token] if token.present?
-    @user_id = authenticate_user(hash_token)[:user_id] if hash_token.present?
 
     @email = Account::Email.from_string(hash_signups[:email]) if hash_signups&.key?(:email)
     @password = Account::Password.from_string(hash_signups[:password]) if hash_signups&.key?(:password)
@@ -34,45 +31,18 @@ class SignupService
     Rails.logger.error(e)
     raise e
   end
-  
-
-  # 本登録用のメールを送信する
-  def activation_email
-    user = UserRepository.find_by_email(@email)
-    raise Unauthorized, '指定のユーザーが見つかりませんでした' if user.blank? || user&.activated
-
-    # アクティベーションメールのリンクのクエリパラメータに入れるJWTを生成する
-    payload = signup_payload(user)
-    auth = generate_token(lifetime: Auth.token_signup_lifetime, payload:) # SessionModuleのメソッド
-    token = auth.token
-    expires_at = Time.zone.at(auth.payload[:exp]) # SessionModuleのメソッド
-
-    # メールの作成と送信
-    ActivationMailer.send_activation_email(email, token, expires_at).deliver
-  end
 
   private
 
-  # アクティベーション用のトークン生成のためのペイロード
-  def signup_payload(user)
-    {
-      sub: user.id,
-      type: 'activation'
-    }
-  end
-
   class << self
-    def signup(signups, token)
+    def signup(signups)
       raise ArgumentError, 'emailがありません' if signups[:signups][:email].blank?
       raise ArgumentError, 'passwordがありません' if signups[:signups][:password].blank?
 
-      service = SignupService.new(signups:, token:)
+      service = SignupService.new(signups:)
       service&.add
 
-      # contract権限がなければメールアドレス登録直後にメールを送る
-      return if token and token[:token] and PermissionService.has_contract_role?(token[:token])
-
-      service&.activation_email 
+      ActivationMailService.activation_email(service.email)
     rescue Account::Email::EmailFormatError => e
       Rails.logger.error(e)
       raise EmailFormatError
--- src/spec/services/signup_service_spec.rb
+++ src/spec/services/signup_service_spec.rb
@@ -6,20 +6,15 @@ require 'faker'
 
 describe SignupService do
   include SessionModule
+  before do
+    travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
+    allow(ActivationMailService).to receive(:activation_email)
+  end
   describe '#add' do
     context '正常系' do
       context '正しいメールアドレスとパスワードを受け取った場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-
         let!(:email) { Faker::Internet.email }
         let!(:password) { 'P@ssw0rd' }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -30,7 +25,7 @@ describe SignupService do
         end
 
         it 'usersにメールアドレスとハッシュ化されたパスワードがインサートされること' do
-          service = SignupService.new(signups:, token:)
+          service = SignupService.new(signups:)
           user = service.add
           expect(user.email).to eq(email)
           expect(user.authenticate(password)).to be_truthy
@@ -39,17 +34,8 @@ describe SignupService do
     end
 
     context '異常系' do
-      before do
-        travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-      end
-
       context 'メールアドレスが引数にない場合' do
         let!(:password) { 'P@ssw0rd' }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -60,17 +46,12 @@ describe SignupService do
         end
 
         it 'ArgumentErrorがスローされること' do
-          expect { SignupService.new(signups:, token:) }.to raise_error(ArgumentError, 'emailがありません')
+          expect { SignupService.new(signups:) }.to raise_error(ArgumentError, 'emailがありません')
         end
       end
 
       context 'パスワードが引数にない場合' do
         let!(:email) { Faker::Internet.email }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -81,18 +62,13 @@ describe SignupService do
         end
 
         it 'ArgumentErrorがスローされること' do
-          expect { SignupService.new(signups:, token:) }.to raise_error(ArgumentError, 'passwordがありません')
+          expect { SignupService.new(signups:) }.to raise_error(ArgumentError, 'passwordがありません')
         end
       end
 
       context 'メールアドレスが既に存在する場合' do
         let!(:email) { Faker::Internet.email }
         let!(:password) { 'P@ssw0rd' }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -104,78 +80,8 @@ describe SignupService do
         let!(:user) { create(:user, email:, password:) }
 
         it 'DuplicateEntryがスローされること' do
-          service = SignupService.new(signups:, token:)
+          service = SignupService.new(signups:)
           expect { service.add }.to raise_error(SignupService::DuplicateEntry)
-        end
-      end
-    end
-  end
-
-  describe '#activation_email' do
-    context '正常系' do
-      let!(:mailer) { double('ActionMailer') }
-      before do
-        allow(mailer).to receive(:deliver)
-        allow(ActivationMailer).to receive(:send_activation_email).and_return(mailer)
-      end
-
-      context '存在するuserかつアクティベーションされていないuserの場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-
-        let!(:email) { Faker::Internet.email }
-        let!(:password) { 'P@ssw0rd' }
-        let!(:user) { create(:user, email:, password:) }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
-        let!(:signups) do
-          {
-            signups: {
-              email: email,
-              password: password
-            }
-          }
-        end
-
-        it 'メールが送られること' do
-          service = SignupService.new(signups:, token:)
-          service.activation_email
-          expect(ActivationMailer).to have_received(:send_activation_email)
-          expect(mailer).to have_received(:deliver)
-        end
-      end
-    end
-
-    context '異常系' do
-      context 'userがアクティベート済の場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-
-        let!(:email) { Faker::Internet.email }
-        let!(:password) { 'P@ssw0rd' }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
-        let!(:signups) do
-          {
-            signups: {
-              email: email,
-              password: password
-            }
-          }
-        end
-        let!(:user) { create(:user, email:, password:, activated: true) }
-
-        it 'UserNotFoundがスローされること' do
-          service = SignupService.new(signups:, token:)
-          expect { service.activation_email }.to raise_error(SignupService::Unauthorized)
         end
       end
     end
@@ -183,80 +89,21 @@ describe SignupService do
 
   describe '#self.sigunp' do
     context '正常系' do
-      let!(:mailer) { double('ActionMailer') }
-      before do
-        allow(mailer).to receive(:deliver)
-        allow(ActivationMailer).to receive(:send_activation_email).and_return(mailer)
-      end
-
-      context 'contract権限を持つユーザーが登録した場合' do
-        context '存在するuserかつアクティベーションされていないuserの場合' do
-          before do
-            travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-          end
-
-          let!(:user) { create(:user, activated: true) }
-          let!(:contract_resource) { Resource.find_by(name: 'contract') }
-          let!(:permission) { create(:permission, user_id: user.id, resource_id: contract_resource.id) }
-          let!(:payload) do
-            {
-              sub: user.id,
-              type: 'api'
+      context '存在するuserかつアクティベーションされていないuserの場合' do
+        let!(:email) { Faker::Internet.email }
+        let!(:password) { 'P@ssw0rd' }
+        let!(:signups) do
+          {
+            signups: {
+              email: email,
+              password: password
             }
-          end
-          let!(:auth) { generate_token(payload:) }
-  
-          let!(:email) { Faker::Internet.email }
-          let!(:password) { 'P@ssw0rd'}
-          let!(:token) do
-            {
-              token: auth.token
-            }
-          end
-          let!(:signups) do
-            {
-              signups: {
-                email: email,
-                password: password
-              }
-            }
-          end
-
-          it 'メールが送られないこと' do
-            SignupService.signup(signups, token)
-            expect(ActivationMailer).to_not have_received(:send_activation_email)
-            expect(mailer).to_not have_received(:deliver)
-          end
+          }
         end
-      end
 
-      context 'contract権限を持たないユーザーが登録した場合' do
-        context '存在するuserかつアクティベーションされていないuserの場合' do
-          before do
-            travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-          end
-
-          let!(:email) { Faker::Internet.email }
-          let!(:password) { 'P@ssw0rd'}
-          let!(:token) do
-            {
-              token: nil
-            }
-          end
-          let!(:signups) do
-            {
-              signups: {
-                email: email,
-                password: password
-              }
-            }
-          end
-
-          it 'メールが送られること' do
-            SignupService.signup(signups, token)
-            expect(ActivationMailer).to have_received(:send_activation_email)
-            expect(mailer).to have_received(:deliver)
-          end
+        it 'activation_emailが呼ばれること' do
+          SignupService.signup(signups)
+          expect(ActivationMailService).to have_received(:activation_email).with(email)
         end
       end
     end
@@ -264,11 +111,6 @@ describe SignupService do
     context '異常系' do
       context 'emailが引数にない場合' do
         let!(:password) { 'P@ssw0rd' }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -279,17 +121,12 @@ describe SignupService do
         end
 
         it 'ArgumentErrorがスローされること' do
-          expect { SignupService.signup(signups, token) }.to raise_error(ArgumentError, 'emailがありません')
+          expect { SignupService.signup(signups) }.to raise_error(ArgumentError, 'emailがありません')
         end
       end
 
       context 'passwordが引数にない場合' do
         let!(:email) { Faker::Internet.email }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -300,18 +137,13 @@ describe SignupService do
         end
 
         it 'ArgumentErrorがスローされること' do
-          expect { SignupService.signup(signups, token) }.to raise_error(ArgumentError, 'passwordがありません')
+          expect { SignupService.signup(signups) }.to raise_error(ArgumentError, 'passwordがありません')
         end
       end
 
       context 'emailのフォーマットが不正な場合' do
         let!(:email) { 'test' }
         let!(:password) { 'P@ssw0rd' }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -322,18 +154,13 @@ describe SignupService do
         end
 
         it 'EmailFormatErrorがスローされること' do
-          expect { SignupService.signup(signups, token) }.to raise_error(SignupService::EmailFormatError)
+          expect { SignupService.signup(signups) }.to raise_error(SignupService::EmailFormatError)
         end
       end
 
       context 'passwordのフォーマットが不正な場合' do
         let!(:email) { Faker::Internet.email }
         let!(:password) { 'test' }
-        let!(:token) do
-          {
-            token: nil
-          }
-        end
         let!(:signups) do
           {
             signups: {
@@ -344,7 +171,7 @@ describe SignupService do
         end
 
         it 'PasswordFormatErrorがスローされること' do
-          expect { SignupService.signup(signups, token) }.to raise_error(SignupService::PasswordFormatError)
+          expect { SignupService.signup(signups) }.to raise_error(SignupService::PasswordFormatError)
         end
       end
     end

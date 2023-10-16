0e27501d6bd534b22f8e6f67e68763be146c219f
goppy <tacumin.goppy.06@gmail.com>
2023-10-16 19:52:10 +0900

    change

--- src/spec/repositories/permission_repository_spec.rb
+++ src/spec/repositories/permission_repository_spec.rb
@@ -5,111 +5,41 @@ require 'rspec-rails'
 require 'faker'
 
 describe PermissionRepository do
-  describe '#upsert' do
+  before do
+    travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
+  end
+  let!(:user) { create(:user, activated: true) }
+
+  describe '#self.create' do
     context '正常系' do
       context 'user_idと存在するpermissionsを受け取った場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-        let!(:user) { create(:user, activated: true) }
         let!(:permissions) do
           {
-            resource: [
-              'user',
-              'contract'
-            ]
+            resource: 'user'
           }
         end
 
         it 'user_idとresource_idでインサートされること' do
-          PermissionRepository.upsert(user.id, permissions)
+          PermissionRepository.create(user.id, permissions)
           actual = Permission.where(user_id: user.id)
-          resource_1 = Resource.find_by(name: permissions[:resource][0])
-          resource_2 = Resource.find_by(name: permissions[:resource][1])
-          expect(actual.length).to eq(2)
-          expect(actual).to include( have_attributes(resource_id: resource_1.id ) )
-          expect(actual).to include( have_attributes(resource_id: resource_2.id ) )
+          resource = Resource.find_by(name: permissions[:resource])
+          expect(actual.length).to eq(1)
+          expect(actual).to include( have_attributes(resource_id: resource.id ) )
         end
       end
 
       context 'user_idと存在しないpermissionsを受け取った場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-        let!(:user) { create(:user, activated: true) }
         let!(:permissions) do
           {
-            resource: [
-              'user',
-              'contract',
-              'hoge'
-            ]
+            resource: 'hoge'
           }
         end
 
-        it 'user_idとresource_idでインサートされること' do
-          PermissionRepository.upsert(user.id, permissions)
+        it 'user_idとresource_idでインサートされないこと' do
+          PermissionRepository.create(user.id, permissions)
           actual = Permission.where(user_id: user.id)
-          resource_1 = Resource.find_by(name: permissions[:resource][0])
-          resource_2 = Resource.find_by(name: permissions[:resource][1])
-          expect(actual.length).to eq(2)
-          expect(actual).to include( have_attributes(resource_id: resource_1.id ) )
-          expect(actual).to include( have_attributes(resource_id: resource_2.id ) )
-        end
-      end
-
-      context 'contractがread_promptに変わった場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-        let!(:user) { create(:user, activated: true) }
-        let!(:db_permissions) do
-          create(:permission, user_id: user.id, resource_id: 1)
-          create(:permission, user_id: user.id, resource_id: 2)
-        end
-        let!(:permissions) do
-          {
-            resource: [
-              'user',
-              'read_prompt'
-            ]
-          }
-        end
-
-        it 'userはそのままでcontractがread_promptに変更されること' do
-          PermissionRepository.upsert(user.id, permissions)
-          actual = Permission.where(user_id: user.id)
-          resource_1 = Resource.find_by(name: permissions[:resource][0])
-          resource_2 = Resource.find_by(name: permissions[:resource][1])
-          expect(actual.length).to eq(2)
-          expect(actual).to include( have_attributes(resource_id: resource_1.id ) )
-          expect(actual).to include( have_attributes(resource_id: resource_2.id ) )
-        end
-      end
-
-      context 'contractを削除した場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-        let!(:user) { create(:user, activated: true) }
-        let!(:db_permissions) do
-          create(:permission, user_id: user.id, resource_id: 1)
-          create(:permission, user_id: user.id, resource_id: 2)
-        end
-        let!(:permissions) do
-          {
-            resource: [
-              'user'
-            ]
-          }
-        end
-
-        it 'userはそのままでcontractが削除されること' do
-          PermissionRepository.upsert(user.id, permissions)
-          actual = Permission.where(user_id: user.id)
-          resource_1 = Resource.find_by(name: permissions[:resource][0])
-          expect(actual.length).to eq(1)
-          expect(actual).to include( have_attributes(resource_id: resource_1.id ) )
+          resource = Resource.find_by(name: permissions[:resource])
+          expect(actual.length).to eq(0)
         end
       end
     end
@@ -118,13 +48,11 @@ describe PermissionRepository do
   describe '#show' do
     context '正常系' do
       context 'user_idを受け取った場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-        let!(:user) { create(:user, activated: true) }
+        let!(:user_resource) { Resource.find_by(name: 'user') }
+        let!(:contract_resource) { Resource.find_by(name: 'contract') }
         let!(:db_permissions) do
-          create(:permission, user_id: user.id, resource_id: 1)
-          create(:permission, user_id: user.id, resource_id: 2)
+          create(:permission, user_id: user.id, resource_id: user_resource.id)
+          create(:permission, user_id: user.id, resource_id: contract_resource.id)
         end
 
         it 'user_idに紐づくPermissionオブジェクトが返ること' do
@@ -132,6 +60,32 @@ describe PermissionRepository do
           expect(actual.length).to eq(2)
           expect(actual).to include('user', 'contract')
         end
+      end
+    end
+  end
+
+  describe '#self.delete' do
+    context 'contractを削除した場合' do
+      let!(:user_resource) { Resource.find_by(name: 'user') }
+      let!(:contract_resource) { Resource.find_by(name: 'contract') }
+      let!(:db_permissions) do
+        create(:permission, user_id: user.id, resource_id: user_resource.id)
+        create(:permission, user_id: user.id, resource_id: contract_resource.id)
+      end
+      let!(:permissions) do
+        {
+          resource: [
+            'user'
+          ]
+        }
+      end
+
+      it 'userはそのままでcontractが削除されること' do
+        PermissionRepository.delete(user.id, permissions)
+        actual = Permission.where(user_id: user.id)
+        resource = Resource.find_by(name: permissions[:resource])
+        expect(actual.length).to eq(1)
+        expect(actual).to include( have_attributes(resource_id: contract_resource.id ) )
       end
     end
   end

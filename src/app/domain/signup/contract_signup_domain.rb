3504a9c04d151d22ef8a603e7f18c7e2c178e636
goppy <tacumin.goppy.06@gmail.com>
2023-10-23 13:40:11 +0900

    add

--- src/app/domain/signup/contract_signup_domain.rb
+++ src/app/domain/signup/contract_signup_domain.rb
@@ -1,60 +1,60 @@
 # frozen_string_literal: true
 
-class ContractSignupDomain
-  include SessionModule
+module Signup
+  class ContractSignupDomain
+    class DuplicateEntry < StandardError; end
+    class EmailFormatError < StandardError; end
+    class PasswordFormatError < StandardError; end
+    class Forbidden < StandardError; end
 
-  class DuplicateEntry < StandardError; end
-  class EmailFormatError < StandardError; end
-  class PasswordFormatError < StandardError; end
-  class Forbidden < StandardError; end
+    attr_reader :email,
+                :password,
+                :user_id,
+                :expires_at
 
-  attr_reader :email,
-              :password,
-              :user_id,
-              :expires_at
+    def initialize(signups: nil)
+      hash_signups = signups[:signups] if signups.present?
 
-  def initialize(signups: nil)
-    hash_signups = signups[:signups] if signups.present?
+      @email = Account::Email.from_string(hash_signups[:email]) if hash_signups&.key?(:email)
+      @password = Account::Password.from_string(hash_signups[:password]) if hash_signups&.key?(:password)
 
-    @email = Account::Email.from_string(hash_signups[:email]) if hash_signups&.key?(:email)
-    @password = Account::Password.from_string(hash_signups[:password]) if hash_signups&.key?(:password)
+      freeze
+    end
 
-    freeze
-  end
-
-  # ユーザー情報をDBに登録する
-  def add
-    UserRepository.create(@email, @password)
-  rescue ActiveRecord::RecordNotUnique => e
-    Rails.logger.error(e)
-    raise DuplicateEntry
-  rescue ActiveRecord::RecordInvalid => e
-    Rails.logger.error(e)
-    raise e
-  end
-
-  class << self
-    def signup(signups)
-      raise ArgumentError, 'emailがありません' if signups[:signups][:email].blank?
-      raise ArgumentError, 'passwordがありません' if signups[:signups][:password].blank?
-
-      service = ContractSignupService.new(signups:)
-
-      service&.add
-      target_user = UserRepository.find_by_email(service.email)
-      return if target_user.blank?
-
-      ContractRepository.create(target_user.id)
-      Rails.logger.info("Contract created_by: user_id: #{service.user_id}, target_user: #{target_user.id}")
-    rescue Account::Email::EmailFormatError => e
+    # ユーザー情報をDBに登録する
+    def add
+      UserRepository.create(@email, @password)
+    rescue ActiveRecord::RecordNotUnique => e
       Rails.logger.error(e)
-      raise EmailFormatError
-    rescue Account::Password::PasswordFormatError => e
-      Rails.logger.error(e)
-      raise PasswordFormatError
-    rescue StandardError => e
+      raise DuplicateEntry
+    rescue ActiveRecord::RecordInvalid => e
       Rails.logger.error(e)
       raise e
+    end
+
+    class << self
+      def signup(signups)
+        raise ArgumentError, 'emailがありません' if signups[:signups][:email].blank?
+        raise ArgumentError, 'passwordがありません' if signups[:signups][:password].blank?
+
+        service = ContractSignupDomain.new(signups:)
+
+        service&.add
+        target_user = UserRepository.find_by_email(service.email)
+        return if target_user.blank?
+
+        ContractRepository.create(target_user.id)
+        Rails.logger.info("Contract created_by: user_id: #{service.user_id}, target_user: #{target_user.id}")
+      rescue Account::Email::EmailFormatError => e
+        Rails.logger.error(e)
+        raise EmailFormatError
+      rescue Account::Password::PasswordFormatError => e
+        Rails.logger.error(e)
+        raise PasswordFormatError
+      rescue StandardError => e
+        Rails.logger.error(e)
+        raise e
+      end
     end
   end
 end

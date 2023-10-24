a46fa809dc016fe29fb6eacffee35d3946084a88
goppy <tacumin.goppy.06@gmail.com>
2023-10-24 18:33:56 +0900

    fix

--- src/app/controllers/concerns/error_handlers.rb
+++ src/app/controllers/concerns/error_handlers.rb
@@ -9,6 +9,7 @@ module ErrorHandlers
     rescue_from Signup::SignupError::DuplicateEntry, with: :rescue409
     rescue_from Signup::SignupError::EmailFormatError, with: :rescue422
     rescue_from Signup::SignupError::PasswordFormatError, with: :rescue422
+    rescue_from Activation::ActivationError::Unauthorized, with: :rescue401
     rescue_from PermissionService::Forbidden, with: :rescue403
     rescue_from ActionController::BadRequest, with: :rescue400
     rescue_from ActionController::Unauthorized, with: :rescue401

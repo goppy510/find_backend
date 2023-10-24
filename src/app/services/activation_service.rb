0c9660d8b7ec482ca9d59221bb03be6f8579003c
goppy <tacumin.goppy.06@gmail.com>
2023-10-24 17:28:56 +0900

    change

--- src/app/services/activation_service.rb
+++ src/app/services/activation_service.rb
@@ -2,31 +2,12 @@
 
 # ユーザーがアクティベーションメールのリンクをクリックしたらアカウントを有効化するためのもの
 class ActivationService
-  include SessionModule
-
-  def initialize(token)
-    @token = token
-  end
-
-  # アクティベート
-  def activate
-    authenticate_user_not_activate(@token) # SessionModuleのメソッド
-    raise Unauthorized unless @auth
-
-    # アクティベートする
-    user = UserRepository.find_by_id(@auth[:user_id])
-
-    UserRepository.activate(user)
-  end
-
   class << self
     def activate(token)
       raise ArgumentError, 'tokenがありません' unless token
 
-      service = new(token)
+      service = Activation::ActivationDomain.activate(token)
       service&.activate
     end
   end
 end
-
-class Unauthorized < StandardError; end
--- src/spec/services/activation_service_spec.rb
+++ src/spec/services/activation_service_spec.rb
@@ -7,72 +7,26 @@ require 'faker'
 describe ActivationService do
   include SessionModule
 
-  describe '#activate' do
-    context '正常系' do
-      context '有効なトークンを受け取った場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-
-        let!(:user) { create(:user) }
-        let!(:payload) do
-          {
-            sub: user.id,
-            type: 'activation'
-          }
-        end
-        let!(:auth) { generate_token(payload:, lifetime: Auth.token_signup_lifetime) }
-        let!(:token) { auth.token }
-
-        it 'usersのactivatedがtrueになること' do
-          service = ActivationService.new(token)
-          service.activate
-          expect(User.find(user.id).activated).to be_truthy
-        end
-      end
-    end
-
-    context '異常系' do
-      context '不正なトークンを受け取った場合' do
-        before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
-        end
-
-        let!(:user) { create(:user) }
-        let!(:token) do
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-            .eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
-              .SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-        end
-
-        it 'Unauthorizedがスローされること' do
-          service = ActivationService.new(token)
-          expect { service.activate }.to raise_error(Unauthorized)
-        end
-      end
-    end
-  end
-
   describe '#self.activate' do
     context '正常系' do
-      context '有効なトークンを受け取った場合' do
+      let!(:user) { create(:user, activated: true) }
+      let!(:payload) do
+        {
+          sub: user.id,
+          type: 'api'
+        }
+      end
+      let!(:auth) { generate_token(payload:) }
+      let!(:token) { auth.token }
+
+      context 'tokenが渡された場合' do
         before do
-          travel_to Time.zone.local(2023, 5, 10, 3, 0, 0)
+          allow(Activation::ActivationDomain).to receive(:activate)
         end
 
-        let!(:user) { create(:user) }
-        let!(:payload) do
-          {
-            sub: user.id,
-            type: 'activation'
-          }
-        end
-        let!(:auth) { generate_token(payload:, lifetime: Auth.token_signup_lifetime) }
-        let!(:token) { auth.token }
-
-        it 'usersのactivatedがtrueになること' do
+        it 'Activation::ActivationDomain.activateが呼ばれること' do
           ActivationService.activate(token)
-          expect(User.find(user.id).activated).to be_truthy
+          expect(Activation::ActivationDomain).to have_received(:activate)
         end
       end
     end

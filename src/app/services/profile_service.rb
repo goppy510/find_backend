42f6443f673a33b4bfe3655802c42ee4349592e9
goppy <tacumin.goppy.06@gmail.com>
2023-11-04 23:58:59 +0900

    fix

--- src/app/services/profile_service.rb
+++ src/app/services/profile_service.rb
@@ -1,81 +1,30 @@
 # frozen_string_literal: true
 
 class ProfileService
-  include SessionModule
-
-  attr_reader :name,
-              :profiles,
-              :password
-
-  def initialize(token, profiles: nil, current_password: nil, new_password: nil)
-    hash_profiles = profiles[:profiles] if profiles.present?
-    @user_id = authenticate_user(token)[:user_id]
-    @current_password = current_password if current_password.present?
-    @new_password = new_password if new_password.present?
-    @profiles = {}
-    @profiles[:name] = Account::Name.from_string(hash_profiles[:name]) if hash_profiles&.key?(:name)
-    if hash_profiles&.key?(:phone_number)
-      @profiles[:phone_number] = Account::PhoneNumber.from_string(hash_profiles[:phone_number])
-    end
-    if hash_profiles&.key?(:company_name)
-      @profiles[:company_name] = Account::CompanyName.from_string(hash_profiles[:company_name])
-    end
-    # 以下、ラジオボタンの数値なのでバリデーションしない
-    @profiles[:employee_count] = hash_profiles[:employee_count] if hash_profiles&.key?(:employee_count)
-    @profiles[:industry] = hash_profiles[:industry] if hash_profiles&.key?(:industry)
-    @profiles[:position] = hash_profiles[:position] if hash_profiles&.key?(:position)
-    @profiles[:business_model] = hash_profiles[:business_model] if hash_profiles&.key?(:business_model)
-
-    freeze
-  end
-
-  # プロフィール新規作成
-  def create
-    ProfileRepository.create(@user_id, @profiles)
-  end
-
-  # プロフィール更新
-  def update
-    ProfileRepository.update(@user_id, @profiles)
-  end
-
-  # プロファイル表示
-  def show
-    ProfileRepository.show(@user_id)
-  end
-
   class << self
+    include SessionModule
+
     def create(token, profiles)
       raise ArgumentError, 'tokenがありません' if token.blank?
       raise ArgumentError, 'profilesがありません' if profiles.blank?
 
-      service = new(token, profiles:)
-      service&.create
+      user_id = authenticate_user(token)[:user_id]
+      Profiles::ProfileDomain.create(user_id, profiles)
     end
 
     def update(token, profiles)
       raise ArgumentError, 'tokenがありません' if token.blank?
       raise ArgumentError, 'profilesがありません' if profiles.blank?
 
-      service = new(token, profiles:)
-      service&.update
+      user_id = authenticate_user(token)[:user_id]
+      Profiles::ProfileDomain.update(user_id, profiles)
     end
 
     def show(token)
       raise ArgumentError, 'tokenがありません' if token.blank?
 
-      res = new(token)&.show
-      {
-        name: res[:full_name],
-        phone_number: res[:phone_number],
-        company_name: res[:company_name],
-        employee_count: EmployeeCount.find(res[:employee_count_id]).name,
-        industry: Industry.find(res[:industry_id]).name,
-        position: Position.find(res[:position_id]).name,
-        business_model: BusinessModel.find(res[:business_model_id]).name
-      }
+      user_id = authenticate_user(token)[:user_id]
+      Profiles::ProfileDomain.show(user_id)
     end
   end
 end
-
-class Unauthorized < StandardError; end

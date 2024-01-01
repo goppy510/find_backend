d21a7d3f24fcf83afac061c23479080aa8429732
goppy <tacumin.goppy.06@gmail.com>
2024-01-02 02:12:24 +0900

    fix

--- src/spec/factories/prompts.rb
+++ src/spec/factories/prompts.rb
@@ -6,6 +6,7 @@ require 'faker'
 FactoryBot.define do
   factory :prompt do
     uuid { SecureRandom.uuid }
+    association :contract
     about { Faker::Lorem.sentence }
     title { Faker::Lorem.sentence }
     input_example { Faker::Lorem.sentence }
@@ -19,8 +20,6 @@ FactoryBot.define do
     after(:build) do |prompt|
       prompt.category ||= 
         Category.any? ? Category.order(Arel.sql('RAND()')).first : create(:category)
-      prompt.contract ||=
-        Contract.any? ? Contract.order(Arel.sql('RAND()')).first : create(:contract)
       prompt.generative_ai_model ||= 
         GenerativeAiModel.any? ? GenerativeAiModel.order(Arel.sql('RAND()')).first : create(:generative_ai_model)
     end

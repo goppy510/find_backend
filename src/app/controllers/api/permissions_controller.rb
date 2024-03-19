1be0d4889469f46e92aaf6095b4dcbb3abed68e6
goppy <tacumin.goppy.06@gmail.com>
2024-03-19 23:21:13 +0900

    change

--- src/app/controllers/api/permissions_controller.rb
+++ src/app/controllers/api/permissions_controller.rb
@@ -7,13 +7,24 @@ module Api
     # 権限追加
     def create
       token = header_token
-      target_user_id = params[:user_id]
+      target_user_email = params[:email]
       permissions = params[:permissions]
-      raise ActionController::BadRequest if target_user_id.blank? || permissions.blank?
+      raise ActionController::BadRequest if target_user_email.blank? || permissions.blank?
 
-      PermissionService.create(token, target_user_id, permissions)
+      PermissionService.create(token, target_user_email, permissions)
       render json: { status: 'success' }, status: :ok
+    rescue Permissions::PermissionError::Forbidden => e
+      Rails.logger.error e
+      rescue403(e)
+    rescue StandardError => e
+      Rails.logger.error e
+      raise ActionController::BadRequest
+    end
 
+    def index
+      token = header_token
+      res = PermissionService.index(token)
+      render json: res, status: :ok
     rescue Permissions::PermissionError::Forbidden => e
       Rails.logger.error e
       rescue403(e)
@@ -38,14 +49,30 @@ module Api
       raise ActionController::BadRequest
     end
 
+    # 権限更新
+    def update
+      token = header_token
+      target_user_email = params[:email]
+      permissions = params[:permissions]
+      raise ActionController::BadRequest if target_user_email.blank?
+
+      PermissionService.update(token, target_user_email, permissions)
+      render json: { status: 'success' }, status: :ok
+    rescue Permissions::PermissionError::Forbidden => e
+      Rails.logger.error e
+      rescue403(e)
+    rescue StandardError => e
+      Rails.logger.error e
+      raise ActionController::BadRequest
+    end
+
     # 権限削除
     def destroy
       token = header_token
-      target_user_id = params[:user_id]
-      permissions = params[:permissions]
-      raise ActionController::BadRequest if target_user_id.blank? || permissions.blank?
+      target_user_email = params[:email]
+      raise ActionController::BadRequest if target_user_email.blank?
 
-      PermissionService.destroy(token, target_user_id, permissions)
+      PermissionService.destroy(token, target_user_email)
       render json: { status: 'success' }, status: :ok
     rescue Permissions::PermissionError::Forbidden => e
       Rails.logger.error e

13fe84b584f9a84ab0103ff430a96349ee937d39
goppy <tacumin.goppy.06@gmail.com>
2024-03-21 11:48:00 +0900

    メンバー登録条件修正

--- src/app/services/signup_service.rb
+++ src/app/services/signup_service.rb
@@ -10,11 +10,13 @@ class SignupService
         contract: Signup::ContractSignupDomain,
         user: Signup::UserSignupDomain
       }
-    
       user_id = authenticate_user(token)[:user_id] if token.present?
-      role = :user if user_id.present? && PermissionService.has_user_role?(user_id)
+      if user_id.present? && PermissionService.has_user_role?(user_id) || PermissionService.has_admin_role?(user_id)
+        role = :user
+      end
       role = :contract if user_id.present? && PermissionService.has_contract_role?(user_id)
       raise Signup::SignupError::Forbidden if role.blank?
+
       domain_class = domain_map[role]
       signups[:signups][:user_id] = user_id if role == :user
       domain_class.signup(signups)

d6782b1a2b0ea92fdf5c4ab3a90f81b1200507ad
goppy <tacumin.goppy.06@gmail.com>
2024-03-25 14:58:44 +0900

    index_all追加

--- src/app/services/prompt_service.rb
+++ src/app/services/prompt_service.rb
@@ -4,19 +4,29 @@ class PromptService
   class << self
     include SessionModule
     include Prompts::PromptError
+    include Permissions::PermissionError
 
     def index(token, page)
       raise ArgumentError, 'tokenがありません' if token.blank?
       raise ArgumentError, 'pageがありません' if page.blank?
 
       user_id = authenticate_user(token)[:user_id]
-      if !PermissionService.has_read_prompt_role?(user_id) && !PermissionService.has_admin_role?(user_id)
-        raise Prompts::PromptError::Forbidden
-      end
-      contract_id = ContractRepository.show(user_id).id
-      raise Prompts::PromptError::Forbidden unless is_own_user?(user_id, contract_id)
+      has_admin = PermissionService.has_admin_role?(user_id)
+      has_read_prompt = PermissionService.has_read_prompt_role?(user_id)
+      raise Prompts::PromptError::Forbidden if !has_read_prompt && !has_admin
 
-      Prompts::PromptDomain.index(contract_id, page)
+      # 管理者権限ならtrue, それ以外は対象ユーザーと自分自身の契約IDが一致しているならtrue
+      is_own_user = has_admin ? true : own_user?(user_id, target_user_id)
+      raise Permissions::PermissionError::Forbidden unless is_own_user
+
+      return Prompts::PromptDomain.index_all(page) if has_admin
+
+      contract_id = ContractRepository.show(user_id).id
+      raise Prompts::PromptError::Forbidden if contract_id.blank?
+
+      Prompts::PromptDomain.index(
+        contract_id, page
+      )
     end
 
     def create(token, prompts)
@@ -27,8 +37,9 @@ class PromptService
       if !PermissionService.has_create_prompt_role?(user_id) && !PermissionService.has_admin_role?(user_id)
         raise Prompts::PromptError::Forbidden
       end
+
       contract_id = ContractRepository.show(user_id).id
-      Prompts::PromptDomain.create(user_id, contract_id, prompts: prompts)
+      Prompts::PromptDomain.create(user_id, contract_id, prompts:)
     end
 
     def update(token, uuid, prompts)
@@ -40,11 +51,12 @@ class PromptService
       if !PermissionService.has_update_prompt_role?(user_id) && !PermissionService.has_admin_role?(user_id)
         raise Prompts::PromptError::Forbidden
       end
-      contract_id = ContractRepository.show(user_id).id
-      raise Prompts::PromptError::Forbidden unless is_own_user?(user_id, contract_id)
-      raise Prompts::PromptError::Forbidden unless is_own_prompt?(uuid, contract_id)
 
-      Prompts::PromptDomain.update(user_id, uuid, prompts: prompts)
+      contract_id = ContractRepository.show(user_id).id
+      raise Prompts::PromptError::Forbidden unless own_user?(user_id, contract_id)
+      raise Prompts::PromptError::Forbidden unless own_prompt?(uuid, contract_id)
+
+      Prompts::PromptDomain.update(user_id, uuid, prompts:)
     end
 
     def destroy(token, uuid)
@@ -55,9 +67,10 @@ class PromptService
       if !PermissionService.has_destroy_prompt_role?(user_id) && !PermissionService.has_admin_role?(user_id)
         raise Prompts::PromptError::Forbidden
       end
+
       contract_id = ContractRepository.show(user_id).id
-      raise Prompts::PromptError::Forbidden unless is_own_user?(user_id, contract_id)
-      raise Prompts::PromptError::Forbidden unless is_own_prompt?(uuid, contract_id)
+      raise Prompts::PromptError::Forbidden unless own_user?(user_id, contract_id)
+      raise Prompts::PromptError::Forbidden unless own_prompt?(uuid, contract_id)
 
       Prompts::PromptDomain.destroy(user_id, uuid)
     end
@@ -73,8 +86,8 @@ class PromptService
       end
 
       contract_id = ContractRepository.show(user_id).id
-      raise Prompts::PromptError::Forbidden unless is_own_user?(user_id, contract_id)
-      raise Prompts::PromptError::Forbidden unless is_own_prompt?(uuid, contract_id)
+      raise Prompts::PromptError::Forbidden unless own_user?(user_id, contract_id)
+      raise Prompts::PromptError::Forbidden unless own_prompt?(uuid, contract_id)
 
       # promptデータを取得
       Prompts::PromptDomain.show(user_id, uuid)
@@ -159,13 +172,15 @@ class PromptService
     end
 
     private
+
     # 契約IDとユーザーが正しい紐づけかチェックする
-    def is_own_user?(user_id, contract_id)
-      ContractMembershipRepository.show(user_id, contract_id).present? || ContractRepository.show(user_id).id == contract_id
+    def own_user?(user_id, contract_id)
+      ContractMembershipRepository.show(user_id,
+                                        contract_id).present? || ContractRepository.show(user_id).id == contract_id
     end
 
     # uuidとcontract_idが正しい紐づけかチェックする
-    def is_own_prompt?(uuid, contract_id)
+    def own_prompt?(uuid, contract_id)
       prompt = PromptRepository.show(uuid)
       return false if prompt.blank?
 
